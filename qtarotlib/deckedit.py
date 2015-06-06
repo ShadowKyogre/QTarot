d249d1284cb79990b98ca468d4ece7afb6ba54e7
ShadowKyogre <shadowkyogre@aim.com>
2015-06-06 16:24:49 -0700

    Finish implemention a functional draft of the deck editor. So... many... buttons... but it works!

--- qtarotlib/deckedit.py
+++ qtarotlib/deckedit.py
@@ -3,16 +3,411 @@
 import os
 import argparse
 from PyQt4 import QtGui,QtCore
-from urllib.parse import urlparse
+from lxml import objectify, etree
+from lxml.etree import DocumentInvalid
+
+from urllib.parse import quote as urlquote
 from random import sample,random
-from pyqt_lxml_utils.models import NodeModel
+from collections import OrderedDict as od
 
 from .guiconfig import QTarotConfig
 from .utilities import QDeckEdit
-from .xmlobjects import objectify, parser
+from .xmlobjects import parser, deck_validator 
 from . import APPNAME,APPVERSION,AUTHOR,DESCRIPTION,YEAR,PAGE,EMAIL
 
+DECK_IMAGE_ROLE = QtCore.Qt.UserRole
+NUMBER_ROLE = QtCore.Qt.UserRole + 1
+NORMAL_MEANING_ROLE = QtCore.Qt.UserRole + 2
+REVERSED_MEANING_ROLE = QtCore.Qt.UserRole + 3
+
 #http://www.sacred-texts.com/tarot/faq.htm#US1909
+
+class DeckEditWidget(QtGui.QWidget):
+	def __init__(self, parent=None):
+		super().__init__(parent=parent)
+
+		self.model = QtGui.QStandardItemModel()
+		self.model.setHorizontalHeaderLabels(["Name", "Affiliation"])
+
+		layout = QtGui.QVBoxLayout(self)
+
+		self.cview = QtGui.QTableView()
+		self.cview.verticalHeader().hide()
+		self.cview.horizontalHeader().setStretchLastSection(True)
+		self.cview.setDragDropMode(QtGui.QAbstractItemView.InternalMove)
+		self.cview.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
+		self.cview.setDragEnabled(True)
+		self.cview.setAcceptDrops(True)
+		self.cview.setDropIndicatorShown(True)
+		self.cview.setModel(self.model)
+		self.cview.resizeColumnsToContents()
+
+		self.iview = QtGui.QListView()
+		self.cview.selectionModel().currentChanged.connect(self.descendList)
+
+		self.pvw = ItemPreviewWidget(self.model)
+		self.pvw.layout().setMargin(0)
+		self.pvw.blankOut()
+
+		removeIButton = QtGui.QPushButton(QtGui.QIcon.fromTheme("edit-delete"), "Delete card")
+		removeCButton = QtGui.QPushButton(QtGui.QIcon.fromTheme("edit-delete"), "Delete suite")
+		addIButton = QtGui.QPushButton(QtGui.QIcon.fromTheme("edit-add"), "Add card")
+		addCButton = QtGui.QPushButton(QtGui.QIcon.fromTheme("edit-add"), "Add suite")
+
+		removeIButton.clicked.connect(self.removeItem)
+		removeCButton.clicked.connect(self.removeCategory)
+		addIButton.clicked.connect(self.addItem)
+		addCButton.clicked.connect(self.addCategory)
+
+		self.deck_name = QtGui.QLineEdit()
+		self.deck_name.setPlaceholderText("Deck Name")
+		self.author = QtGui.QLineEdit()
+		self.author.setPlaceholderText("Author")
+		self.source = QtGui.QLineEdit()
+		self.source.setPlaceholderText("Source")
+
+		splitty = QtGui.QSplitter()
+
+		hold_me = QtGui.QWidget()
+		hold_me_layout = QtGui.QVBoxLayout(hold_me)
+		hold_me_layout.setMargin(0)
+
+		hold_me_close = QtGui.QWidget()
+		hold_me_close_layout = QtGui.QVBoxLayout(hold_me_close)
+		hold_me_close_layout.setMargin(0)
+
+		splitty.addWidget(hold_me)
+		splitty.addWidget(hold_me_close)
+		splitty.addWidget(self.pvw)
+
+		layout.addWidget(self.deck_name)
+		layout.addWidget(self.author)
+		layout.addWidget(self.source)
+
+		hold_me_layout.addWidget(self.cview)
+		hold_me_layout.addWidget(addCButton)
+		hold_me_layout.addWidget(removeCButton)
+
+		hold_me_close_layout.addWidget(self.iview)
+		hold_me_close_layout.addWidget(addIButton)
+		hold_me_close_layout.addWidget(removeIButton)
+
+		layout.addWidget(splitty)
+
+		self.setupModel()
+
+	def descendList(self, idx, pidx):
+		if self.iview.model() is not self.model:
+			self.iview.setModel(self.model)
+			self.iview.selectionModel().currentChanged.connect(self.pvw.updatePreview)
+		if idx.column() > 0:
+			idx = idx.sibling(idx.row(), 0)
+		self.iview.setRootIndex(idx)
+
+	def addItem(self):
+		idx = self.cview.currentIndex()
+		if idx.column() > 0:
+			idx = idx.sibling(idx.row(), 0)
+		if idx.isValid():
+			paritem = self.model.itemFromIndex(idx)
+
+			item = QtGui.QStandardItem()
+			item.setText("Edit meh!")
+			item.setFlags(QtCore.Qt.ItemIsDragEnabled|QtCore.Qt.ItemIsEnabled|QtCore.Qt.ItemIsSelectable)
+
+			item.setData("document-edit", DECK_IMAGE_ROLE)
+			item.setData("I <i>am</i> rich!", NORMAL_MEANING_ROLE)
+			item.setData("I <i>am</i> poor!", REVERSED_MEANING_ROLE)
+
+			paritem.appendRow(item)
+			self.iview.setCurrentIndex(item.index())
+
+	def addCategory(self):
+		item = QtGui.QStandardItem()
+		item.setText("Edit meh!")
+		item.setFlags(QtCore.Qt.ItemIsDropEnabled|QtCore.Qt.ItemIsEnabled|QtCore.Qt.ItemIsEditable|QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsUserCheckable)
+		item.setData(QtCore.Qt.Unchecked, QtCore.Qt.CheckStateRole)
+
+		affil = QtGui.QStandardItem()
+		affil.setText("Dunno")
+		affil.setFlags(QtCore.Qt.ItemIsDropEnabled|QtCore.Qt.ItemIsEnabled|QtCore.Qt.ItemIsEditable|QtCore.Qt.ItemIsSelectable)
+
+		self.model.appendRow([item, affil])
+
+	def removeItem(self):
+		idx = self.iview.currentIndex()
+		if idx.isValid():
+			self.pvw.blankOut()
+			paritem = self.model.itemFromIndex(idx.parent())
+			paritem.takeRow(idx.row())
+
+	def removeCategory(self):
+		idx = self.cview.currentIndex()
+		if idx.isValid():
+			btn = QtGui.QMessageBox.warning(self, "Confirm delete category", 
+			                                "Are you sure you want to delete this?", 
+			                                buttons=QtGui.QMessageBox.Yes|QtGui.QMessageBox.No)
+			if btn == QtGui.QMessageBox.Yes:
+				self.pvw.blankOut()
+				self.model.takeRow(idx.row())
+
+	def toXml(self):
+		xmlobj = etree.fromstring('<deck></deck>')
+		xmlobj.attrib['name']=self.deck_name.text()
+		etree.SubElement(xmlobj, 'author').text = self.author.text()
+		etree.SubElement(xmlobj, 'source').text = self.source.text()
+
+		for sidx in range(self.model.rowCount()):
+			suite_item = self.model.item(sidx)
+			suite_el = etree.SubElement(xmlobj, 'suit')
+			suite_el.attrib['affinity'] = self.model.item(sidx, 1).text()
+			suite_el.attrib['name'] = suite_item.text()
+			suite_el.attrib['nosuitname'] = str(True if suite_item.checkState() == QtCore.Qt.Checked else False).lower()
+			for cidx in range(suite_item.rowCount()):
+				card_item = suite_item.child(cidx)
+
+				card_el = etree.SubElement(suite_el, 'card')
+				card_el.attrib['name'] = card_item.text()
+
+				number_el = etree.SubElement(card_el, 'number')
+				number_el.text = card_item.data(NUMBER_ROLE)
+
+				file_el = etree.SubElement(card_el, 'file')
+				file_el.text = urlquote(card_item.data(DECK_IMAGE_ROLE))
+
+				meaning_el = etree.SubElement(card_el, 'meaning')
+				normal_el = etree.SubElement(meaning_el, 'normal')
+				reversed_el = etree.SubElement(meaning_el, 'reversed')
+				normal_el.text = card_item.data(NORMAL_MEANING_ROLE)
+				reversed_el.text = card_item.data(REVERSED_MEANING_ROLE)
+
+				for src_idx in range(card_item.rowCount()):
+					src_item = card_item.child(src_idx)
+					source_el = etree.SubElement(card_el, 'source')
+					source_el.text = src_item.text()
+
+		return xmlobj
+
+	def fromXml(self, xmlobj):
+		self.model.clear()
+		self.deck_name.setText(xmlobj.attrib['name'])
+		self.author.setText(xmlobj.author.text)
+		self.source.setText(xmlobj.source.text)
+
+		for suit_el in xmlobj.iterchildren(tag='suit'):
+			suit_item = QtGui.QStandardItem()
+			suit_item.setText(suit_el.attrib['name'])
+			suit_item.setFlags(QtCore.Qt.ItemIsDropEnabled|QtCore.Qt.ItemIsEnabled|QtCore.Qt.ItemIsEditable|QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsUserCheckable)
+			suit_item.setData(QtCore.Qt.Checked if suit_el.attrib['nosuitname'] else QtCore.Qt.Unchecked, QtCore.Qt.CheckStateRole)
+
+			affil = QtGui.QStandardItem()
+			affil.setText(suit_el.attrib["affinity"])
+			affil.setFlags(QtCore.Qt.ItemIsDropEnabled|QtCore.Qt.ItemIsEnabled|QtCore.Qt.ItemIsEditable|QtCore.Qt.ItemIsSelectable)
+
+			self.model.appendRow([suit_item, affil])
+
+			for card_el in suit_el.iterchildren(tag='card'):
+				card_item = QtGui.QStandardItem()
+				card_item.setText(card_el.attrib['name'])
+				card_item.setFlags(QtCore.Qt.ItemIsDragEnabled|QtCore.Qt.ItemIsEnabled|QtCore.Qt.ItemIsSelectable)
+
+				card_item.setData(card_el.number.text, NUMBER_ROLE)
+				card_item.setData(card_el.file.text, DECK_IMAGE_ROLE)
+				card_item.setData(card_el.meaning.normal.text, NORMAL_MEANING_ROLE)
+				card_item.setData(card_el.meaning.reversed.text, REVERSED_MEANING_ROLE)
+
+				for source_el in card_el.iterchildren(tag='source'):
+					source_item = QtGui.QStandardItem()
+					source_item.setText(source_el.text)
+					card_item.appendRow(source_item)
+
+				suit_item.appendRow(card_item)
+
+	def setupModel(self, filename=None):
+		self.model.clear()
+		self.author.setText("")
+		self.source.setText("")
+		dummy_deck_contents = od((
+		                        ('Graphics', ('gimp', 'mypaint')),
+		                        ('Editors', ('gvim', 'wine')),
+		))
+		for key, val in dummy_deck_contents.items():
+			item = QtGui.QStandardItem()
+			item.setText(key)
+			item.setFlags(QtCore.Qt.ItemIsDropEnabled|QtCore.Qt.ItemIsEnabled|QtCore.Qt.ItemIsEditable|QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsUserCheckable)
+			item.setData(QtCore.Qt.Unchecked, QtCore.Qt.CheckStateRole)
+
+			affil = QtGui.QStandardItem()
+			affil.setText("durp")
+			affil.setFlags(QtCore.Qt.ItemIsDropEnabled|QtCore.Qt.ItemIsEnabled|QtCore.Qt.ItemIsEditable|QtCore.Qt.ItemIsSelectable)
+			for v in val:
+				child_item = QtGui.QStandardItem()
+				child_item.setText(v.upper())
+				child_item.setData('IX', NUMBER_ROLE)
+				child_item.setData(v, DECK_IMAGE_ROLE)
+				child_item.setData("I <i>am</i> rich!", NORMAL_MEANING_ROLE)
+				child_item.setData("I <i>am</i> poor!", REVERSED_MEANING_ROLE)
+				item.appendRow(child_item)
+				child_item.setFlags(QtCore.Qt.ItemIsDragEnabled|QtCore.Qt.ItemIsEnabled|QtCore.Qt.ItemIsSelectable)
+			self.model.appendRow([item, affil])
+
+class ItemPreviewWidget(QtGui.QWidget):
+	def __init__(self, model, parent=None):
+		super().__init__(parent=parent)
+		layout = QtGui.QVBoxLayout(self)
+
+		self.label = QtGui.QLabel()
+		self.edit_ico_name = QtGui.QLineEdit()
+		self.edit_ico_name.setPlaceholderText("Card Theme Filepath")
+		self.edit_card_num = QtGui.QLineEdit()
+		self.edit_card_num.setPlaceholderText("Card Number")
+		self.edit_card_name = QtGui.QLineEdit()
+		self.edit_card_name.setPlaceholderText("Card Name")
+
+		self.combobox = QtGui.QComboBox()
+		self.combobox.setModel(model)
+		self.combobox.currentIndexChanged['int'].connect(self.changeParent)
+		self.edit_ico_name.textEdited.connect(self.editImageText)
+		self.edit_card_name.textEdited.connect(self.editNameText)
+		self.edit_card_num.textEdited.connect(self.editNumText)
+
+		self.normal_meaning = QtGui.QTextEdit()
+		self.normal_meaning.textChanged.connect(self.editNormalText)
+		self.reversed_meaning = QtGui.QTextEdit()
+		self.reversed_meaning.textChanged.connect(self.editReversedText)
+
+		self.sources_list = QtGui.QListView()
+		self.sources_list.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
+
+		self.splitty = QtGui.QSplitter()
+		self.splitty.setOrientation(QtCore.Qt.Vertical)
+
+		self.smallSplitty = QtGui.QSplitter()
+		self.smallSplitty.setOrientation(QtCore.Qt.Horizontal)
+		self.smallSplitty.addWidget(self.normal_meaning)
+		self.smallSplitty.addWidget(self.reversed_meaning)
+
+		sources_widget = QtGui.QWidget()
+		sources_layout = QtGui.QGridLayout(sources_widget)
+		sources_layout.setMargin(0)
+
+		addSButton = QtGui.QPushButton(QtGui.QIcon.fromTheme("edit-add"), "Add Source")
+		addSButton.clicked.connect(self.addSource)
+		removeSButton = QtGui.QPushButton(QtGui.QIcon.fromTheme("edit-delete"), "Remove Source")
+		removeSButton.clicked.connect(self.removeSource)
+
+		sources_layout.addWidget(self.sources_list, 0, 0, 1, 2)
+		sources_layout.addWidget(addSButton, 1, 0)
+		sources_layout.addWidget(removeSButton, 1, 1)
+
+		self.splitty.addWidget(self.smallSplitty)
+		self.splitty.addWidget(sources_widget)
+
+		layout.addWidget(self.combobox)
+		layout.addWidget(self.edit_card_name)
+		layout.addWidget(self.edit_card_num)
+		layout.addWidget(self.edit_ico_name)
+		layout.addWidget(self.label)
+		layout.addWidget(self.splitty)
+		self._idx = None
+
+	def addSource(self):
+		if self._idx is not None and self._idx.isValid():
+			item = self._idx.model().itemFromIndex(self._idx)
+			subitem = QtGui.QStandardItem("Edit me!")
+			item.appendRow(subitem)
+
+	def removeSource(self):
+		to_remove = [QtCore.QPersistentModelIndex(idx) for idx in self.sources_list.selectedIndexes()[::-1]]
+		item = self._idx.model().itemFromIndex(self._idx)
+		for idx in to_remove:
+			item.takeRow(idx.row())
+
+	def editReversedText(self):
+		if self._idx is not None and self._idx.isValid():
+			self.combobox.model().itemFromIndex(self._idx).setData(self.normal_meaning.toHtml(), REVERSED_MEANING_ROLE)
+
+	def editNormalText(self):
+		if self._idx is not None and self._idx.isValid():
+			self.combobox.model().itemFromIndex(self._idx).setData(self.normal_meaning.toHtml(), NORMAL_MEANING_ROLE)
+
+	def editNameText(self, text):
+		if self._idx is not None and self._idx.isValid():
+			self._idx.model().itemFromIndex(self._idx).setText(text)
+
+	def editNumText(self, text):
+		if self._idx is not None and self._idx.isValid():
+			self._idx.model().itemFromIndex(self._idx).setData(text, NUMBER_ROLE)
+
+	def editImageText(self, text):
+		if self._idx is not None and self._idx.isValid():
+			self._idx.model().itemFromIndex(self._idx).setData(text, DECK_IMAGE_ROLE)
+			self.label.setPixmap(QtGui.QIcon.fromTheme(self._idx.data(DECK_IMAGE_ROLE)).pixmap(256, 256))
+
+	def changeParent(self, idx):
+		print("idx is:", self._idx)
+		if self._idx is not None and self._idx.isValid():
+			print(self._idx.isValid(), self._idx.data(0))
+			item = self._idx.model().itemFromIndex(self._idx)
+			orig_parent = item.parent()
+			new_parent = self.combobox.model().item(idx)
+			print(orig_parent, new_parent, orig_parent is new_parent)
+			if orig_parent is not new_parent and new_parent is not None:
+				orig_parent.takeRow(item.row())
+				new_parent.appendRow(item)
+				#self._idx = self.parent().parent().currentIndex() # <- this points to an invalid idx
+				print("current index is now", self._idx, self._idx.data(0) if self._idx is not None else None)
+				print("changed parents")
+
+			print(orig_parent.rowCount())
+			if orig_parent.rowCount() == 0:
+				print("Hiding due to orig parent having no children")
+				self.blankOut()
+		else:
+			print("invalid index or is none")
+			self.blankOut()
+		print("---")
+
+	def blankOut(self):
+		self.label.setPixmap(QtGui.QPixmap(256, 256))
+		self.label.hide()
+		self.edit_ico_name.setText("")
+		self.edit_ico_name.hide()
+		self.edit_card_name.setText("")
+		self.edit_card_name.hide()
+		self.edit_card_num.setText("")
+		self.edit_card_num.hide()
+		self.normal_meaning.setText("")
+		self.reversed_meaning.setText("")
+		self.combobox.hide()
+		self.splitty.hide()
+		self.sources_list.setModel(None)
+		self._idx = None
+
+	def updatePreview(self, idx, pidx):
+		print("Updating widget")
+		#print(idx.isValid(), idx.parent().isValid())
+		if idx.isValid() and idx.parent().isValid():
+			self._idx = idx
+			self.sources_list.setModel(idx.model())
+			self.sources_list.setRootIndex(idx)
+			self.label.setPixmap(QtGui.QIcon.fromTheme(idx.data(DECK_IMAGE_ROLE)).pixmap(256, 256))
+			self.label.show()
+			self.combobox.setCurrentIndex(idx.parent().row())
+			self.combobox.show()
+			self.edit_ico_name.setText(idx.data(DECK_IMAGE_ROLE))
+			self.edit_ico_name.show()
+			self.edit_card_name.setText(idx.data(QtCore.Qt.DisplayRole))
+			self.edit_card_name.show()
+			self.edit_card_num.setText(idx.data(NUMBER_ROLE))
+			self.edit_card_num.show()
+			self.splitty.show()
+
+			self.normal_meaning.setHtml(idx.data(NORMAL_MEANING_ROLE))
+			self.reversed_meaning.setHtml(idx.data(REVERSED_MEANING_ROLE))
+		else:
+			self.combobox.setCurrentIndex(idx.parent().row())
+			self.blankOut()
 
 class QTarotDeckEdit(QtGui.QMainWindow):
 
@@ -21,20 +416,35 @@ class QTarotDeckEdit(QtGui.QMainWindow):
 		self.initUI()
 
 	def newDeck(self):
-		pass
+		self.view.setupModel()
 
 	def openDeck(self, filename=None):
 		if not filename:
 			filename = str(QtGui.QFileDialog.getOpenFileName(self, caption="Open deck", filter="XML (*.xml)"))
 		if filename:
-			print("opening deck def " + filename)
+			try:
+				print("opening deck def " + filename)
+				xmlobj = objectify.parse(filename, parser=parser).getroot()
+				deck_validator.assertValid(xmlobj)
+				self.view.fromXml(xmlobj)
+			except DocumentInvalid as e:
+				print(filename, "was an invalid deck xml file, starting empty.")
+			except OSError as e:
+				print(filename, "doesn't exist, starting empty.")
 
 	def saveDeck(self, filename=None):
 		if not filename:
-			filename=str(QtGui.QFileDialog.getSaveFileName(self, caption="Save Current Reading",
+			filename=str(QtGui.QFileDialog.getSaveFileName(self, caption="Save deck",
 				filter="XML (*.xml)"))
 		if filename:
 			print("saving deck def " + filename)
+			xmlobj = self.view.toXml()
+			tree_string = etree.tostring(xmlobj, pretty_print=True).decode('utf-8')
+			if not filename:
+				filename = QtGui.QFileDialog.getSaveFileName(self, caption="Save deck", filter="*.xml" )
+			if filename:
+				with open(filename, 'w', encoding='utf-8') as f:
+					f.write(tree_string)
 
 	def about(self):
 		QtGui.QMessageBox.about (self, "About {}".format(APPNAME),
@@ -46,26 +456,7 @@ class QTarotDeckEdit(QtGui.QMainWindow):
 	def initUI(self):
 		self.setWindowTitle(app.applicationName())
 		#self.view = QDeckEdit(self)
-		self.view = QtGui.QTreeView(self)
-		blub=objectify.fromstring("""
-		<deck>
-			<author>me</author>
-			<source>yeep</source>
-			<suit name='them' affinity='durp'>
-				<card name='you'>
-					<file>selfie.png</file>
-					<meaning>
-						<normal>normal self</normal>
-						<reversed>a&lt;i&gt;e&lt;/i&gt;a</reversed>
-					</meaning>
-					<source>yeep/you</source>
-				</card>
-			</suit>
-		</deck>
-		""",parser=parser)
-		model = NodeModel(blub)
-		self.view.setModel(model)
-
+		self.view = DeckEditWidget(parent=self)
 		self.setCentralWidget(self.view)
 		self.setDockNestingEnabled(True)
 
@@ -74,10 +465,10 @@ class QTarotDeckEdit(QtGui.QMainWindow):
 		exitAction.setStatusTip('Exit application')
 		exitAction.triggered.connect(self.close)
 
-		newLayAction = QtGui.QAction(QtGui.QIcon.fromTheme('document-new'), 'Create a new deck definition', self)
-		newLayAction.setShortcut('Ctrl+N')
-		newLayAction.setStatusTip('Create a new deck definition')
-		newLayAction.triggered.connect(self.newDeck)
+		newDeckAction = QtGui.QAction(QtGui.QIcon.fromTheme('document-new'), 'Create a new deck definition', self)
+		newDeckAction.setShortcut('Ctrl+N')
+		newDeckAction.setStatusTip('Create a new deck definition')
+		newDeckAction.triggered.connect(self.newDeck)
 
 		saveAction = QtGui.QAction(QtGui.QIcon.fromTheme('document-save'), 'Save', self)
 		saveAction.setShortcut('Ctrl+S')
@@ -108,7 +499,7 @@ class QTarotDeckEdit(QtGui.QMainWindow):
 		menubar = self.menuBar()
 		fileMenu = menubar.addMenu('&File')
 		fileMenu.addAction(exitAction)
-		fileMenu.addAction(newLayAction)
+		fileMenu.addAction(newDeckAction)
 		#fileMenu.addAction(newChooseAction)
 		fileMenu.addAction(openAction)
 		fileMenu.addAction(saveAction)
@@ -116,7 +507,7 @@ class QTarotDeckEdit(QtGui.QMainWindow):
 
 		toolbar = self.addToolBar('Exit')
 		toolbar.addAction(exitAction)
-		toolbar.addAction(newLayAction)
+		toolbar.addAction(newDeckAction)
 		toolbar.addAction(openAction)
 		toolbar.addAction(saveAction)
 		#toolbar.addAction(browsingAction)
