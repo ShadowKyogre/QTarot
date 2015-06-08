99741208c758c3a9016edaff1d2440d0f3b38409
ShadowKyogre <shadowkyogre@aim.com>
2015-06-08 16:16:10 -0700

    Switch deck editor to the new format: XML + markdown in the meanings for each card

---
+++ qtarot-0509-to-0510.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from lxml import etree
+from html2text import HTML2Text
+
+from argparse import ArgumentParser
+import os
+
+ALL_MEANINGS = etree.XPath('//meaning/normal|//meaning/reversed')
+CONVERTER = HTML2Text()
+
+def convert_to_new_format(xmlobj):
+	for meaning_node in ALL_MEANINGS(xmlobj):
+		if meaning_node.text is not None:
+			html_text = meaning_node.text
+			meaning_node.text = CONVERTER.handle(html_text)
+
+aparser = ArgumentParser()
+aparser.add_argument("--output-dir", default=".")
+aparser.add_argument("input_files", nargs='+')
+
+args = aparser.parse_args()
+
+for fname in args.input_files:
+	xmlobj = etree.parse(fname)
+	convert_to_new_format(xmlobj)
+
+	dest_fname = os.path.join(args.output_dir, os.path.basename(fname))
+
+	with open(dest_fname, 'w', encoding='utf-8') as destf:
+		destf.write(etree.tounicode(xmlobj, pretty_print=True))
